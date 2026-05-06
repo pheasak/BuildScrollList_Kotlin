153db44db8019a5f8ee72abf915111e1687fd735
Sakphea <rosrithsakphea@gmail.com>
2026-05-06 10:41:40 +0700

    build list

--- app/src/main/java/com/example/buildscrolllist/MainActivity.kt
+++ app/src/main/java/com/example/buildscrolllist/MainActivity.kt
@@ -1,16 +1,41 @@
 package com.example.buildscrolllist
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.buildscrolllist.data.DataSource
+import com.example.buildscrolllist.model.Affirmation
 import com.example.buildscrolllist.ui.theme.BuildScrollListTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +45,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BuildScrollListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    AffirmationsApp(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +56,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AffirmationsApp(name: String, modifier: Modifier = Modifier) {
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier.fillMaxSize().statusBarsPadding().padding(
+            start = WindowInsets.safeDrawing.asPaddingValues().calculateStartPadding(layoutDirection),
+            end = WindowInsets.safeDrawing.asPaddingValues().calculateStartPadding(layoutDirection)
+        )
+    ) {
+        AffirmationList(
+            affirmationList = DataSource().loadAffirmation()
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier){
+    Card(modifier = modifier) {
+        Column() {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.fillMaxWidth().height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>,modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) {
+            affirmation -> AffirmationCard(affirmation = affirmation,modifier = Modifier.padding(8.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     BuildScrollListTheme {
-        Greeting("Android")
+        AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
     }
 }
